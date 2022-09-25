4e6e309570734c5072692fa0b336e5f9ca2b6c29
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-25 00:51:11 -0400

    lint code

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 // -------------------------------- REQUIREMENTS
 
 const express = require('express');
-const { redirect, get } = require('express/lib/response');
 const app = express();
 const PORT = 8080;
 const bcrypt = require("bcryptjs");
@@ -37,8 +36,8 @@ const users = {};
 // Function which pulls user specific urls
 const urlsForUser = (userId)=> {
   let userUrls = {};
-  for(let url in urlDatabase) {
-    if(urlDatabase[url].userId === userId) {
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userId === userId) {
       userUrls[url] = urlDatabase[url];
     }
   }
@@ -49,20 +48,20 @@ const urlsForUser = (userId)=> {
 const { getUserByEmail } = require('./helpers');
 
 // Generates user id
-function generateRandomString() {
-  let id =[]
-  let alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' ,'v', 'w', 'x', 'y', 'z'];
-  for( let i = 0; i < 6 ; i++) {
-    if(Math.random() > .5) {
-      id.push(Math.floor(Math.random() *10));
+const generateRandomString = ()=> {
+  let id = [];
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' ,'v', 'w', 'x', 'y', 'z'];
+  for (let i = 0; i < 6; i++) {
+    if (Math.random() > .5) {
+      id.push(Math.floor(Math.random() * 10));
     } else {
-      Math.random() > .5 ? id.push(alphabet[Math.floor(Math.random() * 10)].toUpperCase()): id.push(alphabet[Math.floor(Math.random() * 10)]);
+      Math.random() > .5 ? id.push(alphabet[Math.floor(Math.random() * 10)].toUpperCase()) : id.push(alphabet[Math.floor(Math.random() * 10)]);
     }
   }
   return id.join("");
 };
 
-// --------------------------------- Listender
+// --------------------------------- Listener
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -72,15 +71,15 @@ app.listen(PORT, () => {
 
 // Render home page
 app.get("/urls", (req, res) => {
-  const userUrls = urlsForUser(req.session.user_id); 
-  const templateVars = { urls: userUrls, user: users[req.session.user_id] };
+  const userUrls = urlsForUser(req.session.userId);
+  const templateVars = { urls: userUrls, user: users[req.session.userId] };
   res.render("urls_index.ejs", templateVars);
 });
 
 // Render new url page
 app.get("/urls/new", (req, res) => {
-  const templateVars = { user: users[req.session.user_id] };
-  if(templateVars.user) {
+  const templateVars = { user: users[req.session.userId] };
+  if (templateVars.user) {
     res.render("urls_new", templateVars);
   } else {
     res.redirect("/login");
@@ -89,8 +88,8 @@ app.get("/urls/new", (req, res) => {
 
 // Render page of shortened url
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longUrl: urlDatabase[req.params.id].longUrl, user: users[req.session.user_id] };
-  if(urlDatabase[req.params.id].userId === req.session.user_id) {
+  const templateVars = { id: req.params.id, longUrl: urlDatabase[req.params.id].longUrl, user: users[req.session.userId] };
+  if (urlDatabase[req.params.id].userId === req.session.userId) {
     res.render("urls_show", templateVars);
   } else {
     res.sendStatus(403);
@@ -100,11 +99,11 @@ app.get("/urls/:id", (req, res) => {
 // generate shortened url
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
-  urlDatabase[id] = { longUrl: req.body.longUrl, userId: req.session.user_id };
-  if(req.session.user_id) {
+  urlDatabase[id] = { longUrl: req.body.longUrl, userId: req.session.userId };
+  if (req.session.userId) {
     res.redirect(`/urls/${id}`);
   } else {
-    res.send("Please log in to generate a shortened url!")
+    res.send("Please log in to generate a shortened url!");
   }
 });
 
@@ -112,7 +111,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/u/:id", (req, res) => {
   const id = req.params.id;
   const longUrl = urlDatabase[id].longUrl;
-  if(longUrl) {
+  if (longUrl) {
     res.redirect(longUrl);
   } else {
     res.send("Shortened url does not exist");
@@ -122,12 +121,12 @@ app.get("/urls/u/:id", (req, res) => {
 // Delete url
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  if(!urlDatabase[id]) { // Can refactor as a helper function
+  if (!urlDatabase[id]) { // Can refactor as a helper function
     res.sendStatus(403);
   } else {
-    if(urlDatabase[id].userId === req.session.user_id) {
-    delete urlDatabase[id];
-    res.redirect("/urls");
+    if (urlDatabase[id].userId === req.session.userId) {
+      delete urlDatabase[id];
+      res.redirect("/urls");
     } else {
       res.sendStatus(403);
     }
@@ -137,12 +136,12 @@ app.post("/urls/:id/delete", (req, res) => {
 // Edit url
 app.post("/urls/:id/edit", (req, res) => {
   const id = req.params.id;
-  if(!urlDatabase[id]) {
+  if (!urlDatabase[id]) {
     res.sendStatus(403);
   } else {
-    if(urlDatabase[id].userId === req.session.user_id) {
+    if (urlDatabase[id].userId === req.session.userId) {
       urlDatabase[id].longUrl = req.body.longUrl;
-      urlDatabase[id].userId = req.session.user_id;
+      urlDatabase[id].userId = req.session.userId;
       res.redirect(`/urls/${id}`);
     } else {
       res.sendStatus(403);
@@ -156,10 +155,10 @@ app.post("/login", (req, res) => {
   const loginPassword = req.body.userPassword;
   const user = getUserByEmail(loginEmail, users);
   
-  if(user) {
+  if (user) {
     const userPassword = user.password;
-    if(bcrypt.compareSync(loginPassword, userPassword)) {
-      req.session.user_id = user.id;
+    if (bcrypt.compareSync(loginPassword, userPassword)) {
+      req.session.userId = user.id;
       res.redirect("/urls");
     } else {
       res.status(403).send("Invalid Credentials");
@@ -177,10 +176,10 @@ app.post("/logout", (req, res) => {
 
 // render registration page
 app.get("/register", (req, res) => {
-  const templateVars = { user: users[req.session.user_id]}
-  if(!req.session.user_id){
+  const templateVars = { user: users[req.session.userId]};
+  if (!req.session.userId) {
     res.render('urls_registration', templateVars);
-  } else{
+  } else {
     res.redirect("/urls");
   }
 });
@@ -188,34 +187,34 @@ app.get("/register", (req, res) => {
 // creates new user
 app.post("/register", (req, res) => {
   
-  if(!req.body.userEmail || !req.body.userPassword) {  // Check for null input in both username and password
+  if (!req.body.userEmail || !req.body.userPassword) {  // Check for null input in both username and password
     res.sendStatus(400);
-  };
+  }
   
   const user = getUserByEmail(req.body.userEmail, users);
 
-  if(!user) {
+  if (!user) {
     let userId = generateRandomString();
     const password = req.body.userPassword;
     const hashedPassword = bcrypt.hashSync(password, 10);
-    req.session.user_id = userId;
+    req.session.userId = userId;
     
     users[userId] = {
       id: userId,
       email: req.body.userEmail,
       password: hashedPassword
-    }
+    };
     res.redirect("/urls");
   } else {
     res.status(403).send("User email already exists");
   }
-});  
+});
 
 // Render user log in page
 app.get("/login", (req, res) => {
-  const templateVars = { user : users[req.session.user_id] };
+  const templateVars = { user : users[req.session.userId] };
 
-  if(!req.session.user_id) {
+  if (!req.session.userId) {
     res.render("urls_login.ejs", templateVars);
   } else {
     res.redirect("/urls");
--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ const getUserByEmail = function(userEmail, database) {
   for (let user in database) {  // Check if user email is already in database
     if (database[user].email === userEmail) {
       return database[user];
-    } 
+    }
   }
 };
 
